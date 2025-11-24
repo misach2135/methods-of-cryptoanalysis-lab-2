2a95485d5128dfeb12abee852f75aa8e7d24fabd
Nikita Isachenko <misachenko2135@gmail.com>
2025-11-24 21:41:17 +0000

    simplify processor structure

--- include/text_processor.hpp
+++ include/text_processor.hpp
@@ -1,10 +1,10 @@
-#ifndef TEXT_PREPROCESSOR_HPP
-#define TEXT_PREPROCESSOR_HPP
+#ifndef TEXT_PROCESSOR_HPP
+#define TEXT_PROCESSOR_HPP
 
 #include <string>
 #include <algorithm>
 
-namespace text_preprocessor
+namespace text_processor
 {
     void removeSymbols(std::string &text, const std::string &symbols);
     void preprocessText(std::string &text);
--- src/text_processor.cpp
+++ src/text_processor.cpp
@@ -1,14 +1,6 @@
 #include "text_processor.hpp"
 
-void text_preprocessor::removeSymbols(std::string &text, const std::string &symbols)
-{
-    for (auto c : symbols)
-    {
-        std::remove(text.begin(), text.end(), c);
-    }
-}
-
-void text_preprocessor::preprocessText(std::string &text)
+void text_processor::preprocessText(std::string &text)
 {
     // Make text lowercase
     std::transform(text.begin(), text.end(), text.begin(), [](unsigned char c)
@@ -18,5 +10,10 @@ void text_preprocessor::preprocessText(std::string &text)
     std::replace(text.begin(), text.end(), 'ґ', 'г');
 
     // Remove redundant symbols
-    text_preprocessor::removeSymbols(text, "', .:;-");
+    const std::string symbols = "\', .:;-\"";
+
+    for (auto c : symbols)
+    {
+        std::remove(text.begin(), text.end(), c);
+    }
 }
