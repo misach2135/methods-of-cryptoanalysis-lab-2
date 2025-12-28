cef9fb2ad96484910dc1e59834b6f6bffac4776f
Nikita Isachenko <misachenko2135@gmail.com>
2025-12-29 00:14:05 +0200

    add text processing from file

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,28 @@
 #include <text_processor.h>
 
 int main(int argc, char* argv[]) {
-  std::string test = lab2::processText(u8"hello world. Привіт Світ");
+  if (argc != 2) {
+    std::cerr << "Error: Path to the public text is required" << std::endl;
+    return -1;
+  }
+
+  std::string filepath(argv[1]);
+
+  std::ifstream file(filepath);
+
+  if (!file.is_open()) {
+    std::cerr << "Error: Can't open the file." << std::endl;
+    return -1;
+  }
+
+  file.seekg(0, std::ios::end);
+  std::size_t size = file.tellg();
+  file.seekg(0);
+
+  std::string input_text(size, '\0');
+  file.read(input_text.data(), size);
+
+  std::string test = lab2::processText(input_text);
   std::cout << test << std::endl;
   return 0;
 }
